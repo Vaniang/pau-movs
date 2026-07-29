e3fe580e7b8e24bbbf6b52e76a6dd0d0111465b2
Vaniang <116418019+Vaniang@users.noreply.github.com>
2026-07-29 17:06:40 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker for PAUMOVision
-const CACHE_NAME = 'pauvision-v57'; // Bumped to v11
+const CACHE_NAME = 'pauvision-v58'; // Bumped to v11
 const urlsToCache = [
   './',
   './index.html',
